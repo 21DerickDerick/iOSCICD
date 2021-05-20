67cec630c532ddbb7eb682bf4c13f64cd3361f51
Derick <Derick.Derick@rakuten.com>
2021-05-20 13:52:07 +0800

    track events with properties and crashes

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Analytics.trackEvent("show_main_view")
+        
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "Has error", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            Analytics.trackEvent("show_last_session_crash_alert")
+        }
     }
 
-
+    @IBAction func didTapCrashButton(_ sender: UIButton) {
+        Analytics.trackEvent("crash_button_tapped",
+                             withProperties: ["property key" : "property value"])
+        Crashes.generateTestCrash()
+    }
 }
 
